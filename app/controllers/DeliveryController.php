920a522459641df328f9a5abb15e3181681ba9a8
issdilshod <dilshod@themembers.com>
2022-05-21 13:56:30 +0300

    Task #4: Add function fast delivery

--- app/controllers/DeliveryController.php
+++ app/controllers/DeliveryController.php
@@ -34,10 +34,10 @@ class DeliveryController extends Controller{
                 if(count($tmp_answer)>0){
                     $vars['data'] = ['price' => $tmp_answer['coefficient']*150, 'date' => $tmp_answer['date'], 'error' => ''];
                 }else{
-                    $vars['data'] = ['error' => 'Something went wrong, try again.', 'price' => '0.00', 'date' => ''];
+                    $vars['data'] = ['error' => 'Something went wrong, try again.', 'price' => '', 'date' => ''];
                 }
             }else{
-                $vars['data'] = ['error' => 'Some fields are empty.', 'price' => '0.00', 'date' => ''];
+                $vars['data'] = ['error' => 'Some fields are empty.', 'price' => '', 'date' => ''];
             }
         }
 
@@ -46,6 +46,28 @@ class DeliveryController extends Controller{
     }
 
     public function fastAction(){
-        //
+        // Init
+        $this->setModels();
+        $vars['entity'] = $this->entityDelivery->getEntity();
+        $vars['data'] = [];
+        $vars['webappConfig'] = $this->configWebApp;
+
+        // on Post sended
+        if (isset($_POST['fast'])){
+            $tmp = $this->ifunc->checkPostValues($vars['entity']);
+            if(count($tmp) == count($vars['entity'])){
+                $tmp_answer = $this->ifunc->sendReq($tmp);
+                if(count($tmp_answer)>0){
+                    $vars['data'] = ['price' => $tmp_answer['price'], 'date' => date('Y-m-d', strtotime(date('Y-m-d') . ' +'.$tmp_answer['date'].' day')), 'error' => ''];
+                }else{
+                    $vars['data'] = ['error' => 'Something went wrong, try again.', 'price' => '', 'date' => ''];
+                }
+            }else{
+                $vars['data'] = ['error' => 'Some fields are empty.', 'price' => '', 'date' => ''];
+            }
+        }
+
+        // Answer
+        echo json_encode($vars['data']);
     }
 }
